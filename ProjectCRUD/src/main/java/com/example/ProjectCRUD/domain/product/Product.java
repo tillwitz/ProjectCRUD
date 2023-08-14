9d9c7f1a822395b302373449bdc131d800d853da
tillwitz <williantillwitz@gmail.com>
2023-08-14 06:00:38 -0300

    CRUD JAVA

--- ProjectCRUD/src/main/java/com/example/ProjectCRUD/domain/product/Product.java
+++ ProjectCRUD/src/main/java/com/example/ProjectCRUD/domain/product/Product.java
@@ -1,9 +1,18 @@
 package com.example.ProjectCRUD.domain.product;
 
-@Table(name="product")
-@Entity(name="product")
-@EqualsAndHashCode(of = "id")
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name ="product") // Tabela product
+@Entity(name="product") //Entidade product
+@EqualsAndHashCode(of = "id") //Indicar para o Spring a Primary Key
 public class Product {
-    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    @Id @GeneratedValue(strategy = GenerationType.UUID) //ID da Tabela, GeneratedValue gerado automaticamente
     private String id;
+    private String name;        //ATRIBUTOS DE CRIAÇÃO DA TABELA DO SQL
+    private Number price_in_cents;
 }
--- ProjectCRUD/src/main/java/com/example/ProjectCRUD/domain/product/ProductRepository.java
+++ ProjectCRUD/src/main/java/com/example/ProjectCRUD/domain/product/ProductRepository.java
@@ -1,2 +1,6 @@
-package com.example.ProjectCRUD.domain.product;public class ProductRepository {
+package com.example.ProjectCRUD.domain.product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, String> {
+
 }
